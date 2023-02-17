4d88eab3584a013e061839dbec0f4b04452095b8
Paul Norling <paulnorling87@gmail.com>
2023-02-17 10:47:18 -0600

    fix?

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -10,13 +10,13 @@ import { getStorage } from "firebase/storage";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCv2jMDc0_95CLGmyrwMvCbpd3yf33QelE",
-  authDomain: "civicly-fa2bf.firebaseapp.com",
-  projectId: "civicly-fa2bf",
-  storageBucket: "civicly-fa2bf.appspot.com",
-  messagingSenderId: "513465780137",
-  appId: "1:513465780137:web:9d24bf5579097f076566b8",
-  measurementId: "G-KFHKDJNDLV"
+  apiKey: "AIzaSyAagF_9ahjBsr-QGHMr0WzZq7_JL-iCg_g",
+  authDomain: "civicly-a706b.firebaseapp.com",
+  projectId: "civicly-a706b",
+  storageBucket: "civicly-a706b.appspot.com",
+  messagingSenderId: "449108880437",
+  appId: "1:449108880437:web:cf93fae1a6beb66fdb157c",
+  measurementId: "G-RKEKF4T8VC"
 };
 
 // Initialize Firebase
